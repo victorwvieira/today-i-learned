219f94c919222761bbe0311ba08c21cd03cbaad0
Victor Vieira <victorvieira@Victors-MBP.Home>
2020-04-06 18:58:11 +0100

    TIL - Spread Operator

--- javascript/README.md
+++ javascript/README.md
@@ -11,3 +11,12 @@ console.log('initial array: ', myArray);
 const reverseArray = myArray.reverse()
 console.log('reverse array: ', reverseArray);
 ```
+
+### [Spread Operator](https://github.com/waisbek/today-i-learned/blob/feature/javascript/javascript/array.js)
+
+###### Examples
+
+* ##### Te correct way to copy an array (including a new value or not) is using Spread Operator [...arrayVariable]
+```
+const newArrayRightWay = [...myArray02, 5]
+```
--- javascript/array.js
+++ javascript/array.js
@@ -1,6 +1,19 @@
 // Array.reverse()
-const myArray = [0, 1, 2, 3, 4]
-console.log('initial array: ', myArray);
+const myArray01 = [0, 1, 2, 3, 4]
+console.log('initial array: ', myArray01);
 
-const reverseArray = myArray.reverse()
+const reverseArray = myArray01.reverse()
 console.log('reverse array: ', reverseArray);
+
+// Spread Operator (copy array)
+const myArray02 = [0, 1, 2, 3, 4]
+const newArrayWrongWay = myArray02
+newArrayWrongWay.push(5)
+
+console.log('newArrayWrongWay: ', newArrayWrongWay);
+console.log('myArray: ', myArray02);
+
+const newArrayRightWay = [...myArray02, 5]
+
+console.log('newArrayRightWay: ', newArrayRightWay);
+console.log('myArray02: ', myArray02);
